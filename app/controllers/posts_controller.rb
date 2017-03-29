ebbd6058cf1ddacf90c703883205264eeb357e71
xiejianan <xiejianan86@gmail.com>
2017-03-29 19:53:37 +0800

    all

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,34 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+  end
+
+
+  def update
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.user = current_user
+
+      if @post.update(post_params)
+    redirect_to account_posts_path, notice: "編輯成功"
+  else
+    render :edit
+  end
+    end
+
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "文章删除成功！"
+    redirect_to groups_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
